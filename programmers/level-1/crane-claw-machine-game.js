836339b7b6d9945d463585f1378e6a63fe1151ba
honghyeong <scarletgold0601@gmail.com>
2024-04-02 23:13:10 +0900

    ps

--- programmers/level-1/crane-claw-machine-game.js
+++ programmers/level-1/crane-claw-machine-game.js
@@ -11,11 +11,50 @@
 
  */
 function main() {
-  console.log(solution());
+  console.log(
+    solution(
+      [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 3],
+        [0, 2, 5, 0, 1],
+        [4, 2, 4, 4, 2],
+        [3, 5, 1, 3, 1],
+      ],
+      [1, 5, 3, 5, 1, 2, 1, 4]
+    )
+  );
 }
 main();
 
 function solution(board, moves) {
-  var answer = 0;
+  const map = {};
+  for (let i = 1; i <= board.length; i++) {
+    map[i] = [];
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    board[i].forEach((value, idx) => {
+      if (value != 0) {
+        map[idx + 1].unshift(value);
+      }
+    });
+  }
+
+  const basket = [];
+  let answer = 0;
+
+  moves.forEach((move) => {
+    let doll = 0;
+    if (map[move].length == 0) return;
+
+    doll = map[move].pop();
+    if (basket.length > 0 && basket[basket.length - 1] == doll) {
+      basket.pop();
+      answer += 2;
+    } else {
+      basket.push(doll);
+    }
+  });
+
   return answer;
 }
